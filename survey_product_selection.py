fa9fb2190c74642025ef09350c3fd23834f23338
bibs02 <bea.branco1@gmail.com>
2024-06-10 15:40:51 +0200

    added comments to final version

--- survey_product_selection.py
+++ survey_product_selection.py
@@ -1,20 +1,21 @@
+#import relevant libraries
 import pandas as pd
 import numpy as np
 
+# read data
+df=pd.read_csv('compiled_data.csv')
 
-df=pd.read_csv('compiled_useful_data.csv')
-
-# Step 1: Get frequency counts of ASINs
+# get frequency counts of ASINs (product identifiers)
 asin_counts = df['asin'].value_counts()
 
-# Step 2: Sort ASIN counts in descending order
+# sort ASIN counts in descending order
 sorted_counts = asin_counts.sort_values(ascending=False)
 
-# Step 3: Calculate the threshold for the top 5%
+# calculate the threshold for the top 5%
 threshold_index = int(len(sorted_counts) * 0.05)
 threshold_count = sorted_counts.iloc[threshold_index]
 
-# Step 4: Select ASINs with counts greater than or equal to the threshold
+# select ASINs with counts greater than or equal to the threshold
 top_asins = sorted_counts[sorted_counts >= threshold_count].index.tolist()
 
 
@@ -24,7 +25,7 @@ selected_rows.to_csv('top_products.csv')
 
 # top_asins now contains the ASINs that appear in the top 5% most frequently
 
-
+#randomly select 5 of those products
 np.random.seed(5445)
 
 survey_ids = np.random.choice(top_asins, 5, False)
