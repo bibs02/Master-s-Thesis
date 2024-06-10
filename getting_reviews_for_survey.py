6c144dda239744df0048065ffc77b979ec42a98b
bibs02 <bea.branco1@gmail.com>
2024-06-10 16:06:46 +0200

    added comments to final version

--- getting_reviews_for_survey.py
+++ getting_reviews_for_survey.py
@@ -1,11 +1,13 @@
+#import relevant library
 import pandas as pd
 
 
-
-
+#read data
 df=pd.read_csv('top_products.csv') 
 
+#randomly sample 10 reviews for each of the products that will be on survey
 
+#product 1
 prod = df[df['asin'] == 'B004MMEHOM']
 random_rows = prod.sample(n=10,random_state=3011)
 
@@ -14,7 +16,7 @@ for index, row in random_rows.iterrows():
     print("Overall:", row['overall'])
     print()
 
-
+#product 2
 prod = df[df['asin'] == 'B000FX81V2']
 random_rows = prod.sample(n=10,random_state=3011)
 
@@ -23,6 +25,7 @@ for index, row in random_rows.iterrows():
     print("Overall:", row['overall'])
     print()
 
+#product 3
 prod = df[df['asin'] == 'B00H1B5E3E'] 
 random_rows = prod.sample(n=10,random_state=3011)
 for index, row in random_rows.iterrows():
@@ -30,6 +33,7 @@ for index, row in random_rows.iterrows():
     print("Overall:", row['overall'])
     print()
 
+#product 4
 prod = df[df['asin'] == 'B00T62RK1U']
 random_rows = prod.sample(n=10,random_state=3011)
 for index, row in random_rows.iterrows():
@@ -37,6 +41,7 @@ for index, row in random_rows.iterrows():
     print("Overall:", row['overall'])
     print()
 
+#product 5
 prod = df[df['asin'] == 'B002IVHQ5Q']
 random_rows = prod.sample(n=10,random_state=3011)
 for index, row in random_rows.iterrows():
